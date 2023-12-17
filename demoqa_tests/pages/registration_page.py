d65369f3d2967cf12d068c064e37c966be1361cb
Sergey Gavrilenko <serezagavrilenko@gmail.com>
2023-12-17 18:41:24 +0300

    Выполнил часть ДЗ 2

--- demoqa_tests/pages/registration_page.py
+++ demoqa_tests/pages/registration_page.py
@@ -1,4 +1,4 @@
-from selene import browser, be, by, have
+from selene import browser, have, be, by
 from demoqa_tests import resource
 
 
@@ -7,95 +7,47 @@ class RegistrationPage:
         browser.open('/automation-practice-form')
         return self
 
-    def fill_first_name(self, value):
-        browser.element('#firstName').should(be.blank).type(value)
-        return self
-
-    def fill_last_name(self, value):
-        browser.element('#lastName').should(be.blank).type(value)
-        return self
-
-    def fill_user_email(self, value):
-        browser.element('#userEmail').should(be.blank).type(value)
-        return self
-
-    def gender_selection(self, value):
-        if value == "Male":
-            browser.element('[for="gender-radio-1"]').click()
-        elif value == "Female":
-            browser.element('[for="gender-radio-2"]').click()
+    def register(self, user):
+        browser.element('#firstName').type(user.first_name)
+        browser.element('#lastName').type(user.last_name)
+        browser.element('#userEmail').type(user.user_email)
+        if user.user_gender == 'Male':
+            browser.element('[for="gender-radio-1"]').double_click()
+        elif user.user_gender == "Female":
+            browser.element('[for="gender-radio-2"]').double_click()
         else:
-            browser.element('[for="gender-radio-3"]').click()
-        return self
-
-    def fill_user_phone_number(self, value):
-        browser.element('#userNumber').type(value)
-        return self
-
-    def fill_date_of_birth(self, year, month, day):
+            browser.element('[for="gender-radio-3"]').double_click()
+        browser.element('#userNumber').type(user.user_phone_number)
         browser.element('#dateOfBirthInput').click()
-        browser.element('.react-datepicker__month-select').click().element(by.text(month)).click()
-        browser.element('.react-datepicker__year-select').click().element(by.text(year)).click()
-        browser.element(f'.react-datepicker__day--0{day}').click()
-
-    def select_user_subject(self, value):
-        browser.element('#subjectsInput').type(value).press_enter()
-        return self
-
-    def user_hobby_checkbox(self, value):
-        if value == "Sports":
-            browser.element("[for='hobbies-checkbox-1']").click()
-        elif value == "Reading":
-            browser.element("[for='hobbies-checkbox-2']").click()
-        elif value == "Music":
-            browser.element("[for='hobbies-checkbox-3']").click()
-        return self
-
-    def user_picture(self, value):
-        browser.element('#uploadPicture').send_keys(resource.path(value))
-        return self
-
-    def user_current_adress(self, value):
-        browser.element('#currentAddress').should(be.blank).type(value)
-        return self
-
-    def user_state(self, value):
-        browser.element('#react-select-3-input').type(value).press_enter()
-        return self
-
-    def user_city(self, value):
-        browser.element('#react-select-4-input').type(value).press_enter()
-        return self
-
-    def submit_the_form(self):
+        browser.element('.react-datepicker__month-select').click().element(by.text(user.month)).click()
+        browser.element('.react-datepicker__year-select').click().element(by.text(user.year)).click()
+        browser.element(f'.react-datepicker__day--0{user.day}').click()
+        browser.element('#subjectsInput').type(user.user_subject).press_enter()
+        if user.user_hobby == 'Sports':
+            browser.element('[for="hobbies-checkbox-1"]').click()
+        elif user.user_hobby == 'Reading':
+            browser.element('[for="hobbies-checkbox-2"]').click()
+        elif user.user_hobby == 'Music':
+            browser.element('[for="hobbies-checkbox-3"]').click()
+        browser.element('#uploadPicture').send_keys(resource.path(user.user_picture))
+        browser.element('#currentAddress').should(be.blank).type(user.user_current_address)
+        browser.element('#react-select-3-input').type(user.user_state).press_enter()
+        browser.element('#react-select-4-input').type(user.user_city).press_enter()
         browser.element('#submit').press_enter()
-        return self
 
-    def should_registered_user_with(
-            self,
-            full_name,
-            email,
-            gender,
-            number,
-            date_of_birth,
-            subjects,
-            hobbies,
-            file,
-            current_address,
-            state_and_city,
-    ):
+    def should_registered_user_with(self, user):
         browser.element('.table').all('td').even.should(
             have.exact_texts(
-                full_name,
-                email,
-                gender,
-                number,
-                date_of_birth,
-                subjects,
-                hobbies,
-                file,
-                current_address,
-                state_and_city,
+                f'{user.first_name} {user.last_name}',
+                user.user_email,
+                user.user_gender,
+                user.user_phone_number,
+                f'{user.day} {user.month},{user.year}',
+                user.user_subject,
+                user.user_hobby,
+                user.user_picture,
+                user.user_current_address,
+                f'{user.user_state} {user.user_city}',
             )
         )
         return self
--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,33 +1,9 @@
 from demoqa_tests.pages.registration_page import RegistrationPage
+from demoqa_tests.data.users import test_user
 
 
-def test_practice_from():
+def test_registration():
     registration_page = RegistrationPage()
     registration_page.open_registration_page()
-
-    registration_page.fill_first_name('Sergey')
-    registration_page.fill_last_name('Gavrilenko')
-    registration_page.fill_user_email('serezagavrilenko@gmail.com')
-    registration_page.gender_selection('Male')
-    registration_page.fill_user_phone_number(9920454526)
-    registration_page.fill_date_of_birth('2003', 'June', '03')
-    registration_page.select_user_subject('Economics')
-    registration_page.user_hobby_checkbox('Sports')
-    registration_page.user_picture('photo.jpg')
-    registration_page.user_current_adress('Artistic driveway 4, sq.1')
-    registration_page.user_state('NCR')
-    registration_page.user_city('Noida')
-    registration_page.submit_the_form()
-
-    registration_page.should_registered_user_with(
-        'Sergey Gavrilenko',
-        'serezagavrilenko@gmail.com',
-        'Male',
-        '9920454526',
-        '03 June,2003',
-        'Economics',
-        'Sports',
-        'photo.jpg',
-        'Artistic driveway 4, sq.1',
-        'NCR Noida',
-    )
+    registration_page.register(test_user)
+    registration_page.should_registered_user_with(test_user)
