f9cfff571f3ae1d4bb1fbc5e07d205dc4f111903
Sergey Gavrilenko <serezagavrilenko@gmail.com>
2023-12-18 18:37:36 +0300

    подключил allure

--- tests/test_homework.py
+++ tests/test_homework.py
@@ -1,9 +1,14 @@
+import allure
+
 from demoqa_tests.pages.registration_page import RegistrationPage
 from demoqa_tests.data.users import test_user
 
 
 def test_registration():
     registration_page = RegistrationPage()
-    registration_page.open_registration_page()
-    registration_page.register(test_user)
-    registration_page.should_registered_user_with(test_user)
+    with allure.step("Открываем форму регистрации"):
+        registration_page.open_registration_page()
+    with allure.step("Регистрируем пользователя"):
+        registration_page.register(test_user)
+    with allure.step("Проверяем успешность регистрации (введённые данные с отображаемыми)"):
+        registration_page.should_registered_user_with(test_user)
