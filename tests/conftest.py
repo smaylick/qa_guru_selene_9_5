9542fbc38ad3e0df223b03b41f7d8f0d3e57fcdb
Sergey Gavrilenko <serezagavrilenko@gmail.com>
2023-12-18 20:09:24 +0300

    добавил в конфигурацию запуск теста в selenoid

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,30 @@
 import pytest
-from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selene import Browser, Config
+
+from utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
-def browser_management():
+def browser_management(request):
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor=f"https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+
+    browser = Browser(Config(driver))
+
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
@@ -12,5 +32,12 @@ def browser_management():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
-    yield
+
+    yield browser
+
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_html(browser)
+    attach.add_video(browser)
+
     browser.quit()
